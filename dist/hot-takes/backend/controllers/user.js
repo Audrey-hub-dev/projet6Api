23836f49b7f17fdb76b9ef9261bc2f0006b6d513
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-04-17 14:04:16 +0200

    modifications statuts erreurs user.js

--- dist/hot-takes/backend/controllers/user.js
+++ dist/hot-takes/backend/controllers/user.js
@@ -84,7 +84,7 @@ exports.login = (req, res, next) => {
             )
           });
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(400).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
 };
